f18786d8c92530fd6206b4d27d3ec219fde4c260
Felipe <felipevalerioderamos@gmail.com>
2025-01-27 22:46:26 -0300

    normalization of pixels values

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@ fn main() {
 
     let img = image::open("C:/Users/Pichau/Documents/mnist_png/training/6/13.png").unwrap();
     let gray_img = img.to_luma8();
-    let pixels = gray_img.pixels();
 
-    for pix in pixels {
-        println!("{:?}", pix)
+    // Obtém os pixels da imagem
+    let pixels: Vec<f32> = gray_img.pixels()
+        .map(|pix| pix[0] as f32 / 255.0) // Normaliza cada pixel para [0, 1]
+        .collect(); // Coleta os resultados em um Vec<f32>
+
+    // Exibe os valores normalizados
+    for (i, &pixel) in pixels.iter().enumerate() {
+        println!("Pixel {}: {}", i, pixel);
     }
 }
+
+
